6b12f15c5fd30de1946f9f86cd6632a767cf8228
abdullah <abdoatamimi@gmail.com>
2022-05-10 05:08:21 +0300

    expand

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
           <div  v-for="(auction) in auc" :key="auction.item.name">
       <div class=" p-10 w-1/3 float-left">
         <div @click="expand(auction.id)" class="bg-white rounded-md flex flex-col overflow-hidden height cursor-pointer shadow-xl">
-          <img class="w-full h-52" :src="auction.item.photoUrl" />
+          <img class="w-full h-48" :src="auction.item.photoUrl" />
           <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">
               <span class="text-blue-500">name:</span> {{ auction.item.name }}
@@ -16,24 +16,15 @@
             <div class="font-bold text-xl mb-2">
              <span class="text-blue-500">End date:</span>  {{  }}
             </div>
-            <p class="text-gray-700 h-24 border text-base">
-              {{ auction.item.description }}
-            </p>
+           
           </div>
           <div class="ml-1/2">
-            <button 
-            v-if="auction.username != user.userName"
-              @click="bid(auction.id,auction.expireDate)"
-              id="pub-btn"
-              class="ml-5 mt-auto delay-400 duration-300 bg-blue-500 hover:bg-blue-800 text-white py-1 px-4 rounded-full"
-            >
-              bid
-            </button>
+            
             <button
               v-if="auction.username == user.userName"
               @click="deletitem(auction.id)"
               id="delete-btn"
-              class="ml-5 bg-red-500 delay-400 duration-300 hover:bg-red-800 text-white py-1 px-4 rounded-full  "
+              class="ml-5 bg-red-500 delay-400 mb-5 w-96 duration-300 hover:bg-red-800 text-white py-1 px-4 rounded-full  "
             >
               Delete
             </button>
@@ -42,54 +33,7 @@
       </div>
     </div>
      </div>
-     <div
-      class="bg-black bg-opacity-50 left-1/3 top-1/2 -translate-x-1/2 fixed -translate-y-1/2 hidden hello"
-      id="overlay"
-    >
-      <div
-        class="bg-gray-200 max-w-sm py-2 px-3 rounded shadow-xl text-gray-800"
-      >
-        <div class="flex justify-between items-center w-80">
-          <h4 class="text-lg font-bold">Confirm Delete?</h4>
-          <svg
-            @click="close()"
-            class="h-6 w-6 cursor-pointer p-1 hover:bg-gray-300 rounded-full"
-            id="close-modal"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div>
-        <div class="mt-2 text-sm">
-          <label name="date">Enter your bidding price:</label>
-          <input
-            v-model="biddingvalue"
-            class="ml-3 bg-gray-200 border-2 border-blue-500 rounded-md"
-            type="number"
-            id="bid"
-          />
-        </div>
-        <div class="mt-3 flex justify-end space-x-3">
-          <button
-            class="px-3 py-1 rounded hover:bg-red-300 hover:bg-opacity-50 hover:text-red-900"
-            @click="close()"
-          >
-            Cancel
-          </button>
-          <button
-            class="px-3 py-1 bg-red-800 text-gray-200 hover:bg-red-600 rounded"
-            @click="bidding()"
-          >
-            bid
-          </button>
-        </div>
-      </div>
-    </div>
+     
   </div>
 </template>
 
@@ -98,13 +42,9 @@
 import axios from "axios"
 // eslint-disable-next-line no-unused-vars
 import login from "@/components/login.vue";
-import card from "@/components/expand-card.vue"
+
 export default {
   name: 'HomeView',
-  components:{
-     // eslint-disable-next-line vue/no-unused-components
-     card
-  },
   props:['user'],
   data(){
     return{
@@ -113,8 +53,7 @@ export default {
       date:"",
       myaucfilter:false,
       expanditem:"",
-      no:false,
-      items:"ergrgerg"
+      
     }
   },
   created(){
@@ -128,33 +67,10 @@ export default {
         }
       })
       .then((res) => {this.auc = res.data.data
+      console.log(this.auc[4].highestBids.value)
       });  
 },
 methods:{
-      bid(itemid) {
-      const popup = document.querySelector(".hello");
-      popup.style.display = "block";
-      this.currentid = itemid;
-    },
-    close() {
-      const popup = document.querySelector(".hello");
-      popup.style.display = "none";
-    },
-    async bidding() {
-      
-      await axios.post(
-        "https://online-auction0.herokuapp.com/v1/bid",
-        {
-          value:this.biddingvalue ,
-          auctionId:this.currentid,
-        },
-        {
-          headers: {
-            Authorization: "bearer " + window.localStorage.getItem("token"),
-          },
-        }
-      );
-    },
      deletitem(itemid) {
       axios
         .delete(
@@ -185,9 +101,7 @@ methods:{
 }
 </script>
 <style lang="css">
-   .height{
-     height:500px;
-   }
+  
    .home{
      height:500vh;
    }
--- src/views/expandcardview.vue
+++ src/views/expandcardview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home h-screen">
      <div class="w-full">
      <div class="w-3/4 float-left">
       <div style='background-color:rgba(0, 0, 0, 0)'>
@@ -15,14 +15,15 @@
                   <p><span class="text-blue-400 text-xl">expiration date:</span> {{item.expireDate}}</p>
                 </div>
               </div>
-              <p class="h-52 leading-relaxed">{{item.description}}</p>
+              <p class="h-52 leading-relaxed">{{item.item.description}}</p>
               <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
               
                
               </div>
               <div class="flex">
-                <span class="title-font font-medium text-2xl text-gray-900">${{highestbid()}}</span>
-                <button @click="bid()" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">bid</button>
+                <span v-if="highestbid()" class="title-font font-medium text-2xl text-gray-900">${{highestbid()}}</span>
+                
+                <button v-if="user.userName != item.username" @click="bid(item.id)" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">bid</button>
                 
               </div>
             </div>
@@ -32,11 +33,59 @@
      </div>
      <div class="w-1/4 h-screen border-2 border-t-0 border-b-0   float-right mt-24">
      <div v-for="bid in bids " :key="bid.vale">
-         <div class="h-10 w-full mt-5 bg-blue-300">{{bid.value}} {{bids.userName}}</div>
+         
      </div>
         
      </div>
    </div>
+    <div
+      class="bg-black bg-opacity-50 left-1/3 top-1/2 -translate-x-1/2 fixed -translate-y-1/2 hidden hello"
+      id="overlay"
+    >
+      <div
+        class="bg-gray-200 max-w-sm py-2 px-3 rounded shadow-xl text-gray-800"
+      >
+        <div class="flex justify-between items-center w-80">
+          <h4 class="text-lg font-bold">Confirm Delete?</h4>
+          <svg
+            @click="close()"
+            class="h-6 w-6 cursor-pointer p-1 hover:bg-gray-300 rounded-full"
+            id="close-modal"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <div class="mt-2 text-sm">
+          <label name="date">Enter your bidding price:</label>
+          <input
+            v-model="biddingvalue"
+            class="ml-3 bg-gray-200 border-2 border-blue-500 rounded-md"
+            type="number"
+            id="bid"
+          />
+        </div>
+        <div class="mt-3 flex justify-end space-x-3">
+          <button
+            class="px-3 py-1 rounded hover:bg-red-300 hover:bg-opacity-50 hover:text-red-900"
+            @click="close()"
+          >
+            Cancel
+          </button>
+          <button
+            class="px-3 py-1 bg-red-800 text-gray-200 hover:bg-red-600 rounded"
+            @click="bidding()"
+          >
+            bid
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,23 +96,50 @@ import axios from "axios"
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'cardview',
-  props:['expanditem'],
+  props:["user"],
 
     data(){
     return{
         id:"",
         item:"",
         bids:"",
+        currentid:"",
     }
   },
   methods:{
-    bid(){
-        console.log(this.id) 
+     bid(itemid) {
+      const popup = document.querySelector(".hello");
+      popup.style.display = "block";
+      this.currentid = itemid;
+    },
+    close() {
+      const popup = document.querySelector(".hello");
+      popup.style.display = "none";
+    },
+    async bidding() {
+      
+      await axios.post(
+        "https://online-auction0.herokuapp.com/v1/bid",
+        {
+          value:this.biddingvalue ,
+          auctionId:this.currentid,
+        },
+        {
+          headers: {
+            Authorization: "bearer " + window.localStorage.getItem("token"),
+          },
+        }
+      );
     },
     highestbid(){
-        this.bids = this.item.bids
-
+      this.bids = this.item.bids
+      if (this.bids.length != 0){
         return this.item.bids[this.bids.length-1].value
+      }
+      else{
+          return this.item.currentPrice
+      }
+        
     }
   },
   created(){
@@ -79,7 +155,7 @@ export default {
           id:this.id
         },
       ).then(res => {this.item = res.data
-      console.log(this.item)});
+      console.log(this.item.username)});
   }
  
 
